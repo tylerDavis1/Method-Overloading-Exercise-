9da6532d44fb9db6ed3c261514d683e1b7b7c256
tyler davis <tyler.davis432@gmail.com>
2023-11-15 22:34:52 -0500

    complete methods

---
+++ MethodOverloading/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Class1
+    {
+        public static int Add (int x, int y)
+        {
+            return x + y; 
+        }
+
+        public static decimal Add (decimal x , decimal y )
+        {
+            return x + y; 
+        }
+
+        public static string Add (int x , int y , bool withPesos)
+        {
+            if (withPesos)
+            {
+                return $"{x + y} pesos";
+            }else if (withPesos && (x + y) == 1)
+            {
+                return $"{x + y} pesos";
+            }
+            else
+            {
+                return $"{x + y}";
+            }
+            
+        }
+       
+
+        
+    }
+}
